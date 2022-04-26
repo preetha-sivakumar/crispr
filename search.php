7fe49e60bbf7a2ab8474e27bcea53d4a2af062ed
preetha-sivakumar <preethasivakumar111@gmail.com>
2022-04-26 14:51:01 +0530

    first commit

---
+++ search.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+    
+<head>
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <link type="text/css" rel="stylesheet" href="style2.css">
+
+</head>
+<body>
+    <div class="header">
+
+       
+
+
+           <nav>
+               <img src="logoo.png" class="logo">
+               <h2 style="color:white;">CRISPRCAN</h2>
+              <ul class="nav-links">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="tool.php">Tools</a></li>
+                  <li><a href="browse1.php">Browse(VISUALIZATION)</a></li>
+                  <li><a href="search.php">Search</a></li>
+                  <li><a href="wiki.php">Wiki Help</a></li>
+              </ul>
+          </nav>
+
+          <div class="content">
+              <h1>SEARCH</h2>
+              <form>
+                  <input type="text" placeholder="&#x270e; Enter Keyword">
+                  <button type="submit">Find</button>
+               </form>
+
+
+
+           </div>
+</body>
